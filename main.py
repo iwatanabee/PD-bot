e228fa9050f71db7a410c4690b57e9e38d400472
iwatanabee <issei.watanabe0708@gmail.com>
2023-01-10 04:09:02 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+import discord
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+  print("I'm in")
+  print(client.user)
+
+
+@client.event
+async def on_message(message):
+  if message.author != client.user:
+    await message.channel.send(message.content[::-1])
+
+
+my_secret = os.environ['TOKEN']
+client.run(my_secret)
